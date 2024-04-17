137e69ce51b15642cdc6f593dcac825dec995635
golubhattuk01 <148994632+golubhattuk01@users.noreply.github.com>
2024-04-17 22:36:41 +0530

    updating dashboard and footer with logout button

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import {
-  
   FaFacebookSquare,
   FaGithubSquare,
   FaInstagram,
   FaTwitterSquare,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useFirebase } from "../FirebaseSetup/Context";
 
 const Footer = () => {
+  const firebase = useFirebase();
   return (
     <div className="bg-gradient-to-b from-purple-400 to-purple-200  py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-800">
       <div className="ml-7">
-  <h1 className="w-full text-3xl font-bold text-purple-800">KernalKaam</h1>
-  <p className="py-4 text-gray-800 space-x-4">
-    Remember, healing is not a linear journey. Be patient with yourself. Reach out for support when you need it, and always remember that you are not alone on this path to wellness
-  </p>
-  <div className="flex justify-between md:w-[35%] my-6 space-x-4">
-    <FaFacebookSquare size={30} />
-    <FaInstagram size={30} />
-    <FaGithubSquare size={30} />
-    
-  </div>
-</div>
+        <h1 className="w-full text-3xl font-bold text-purple-800">
+          KernalKaam
+        </h1>
+        <p className="py-4 text-gray-800 space-x-4">
+          Remember, healing is not a linear journey. Be patient with yourself.
+          Reach out for support when you need it, and always remember that you
+          are not alone on this path to wellness
+        </p>
+        <div className="flex justify-between md:w-[35%] my-6 space-x-4">
+          <FaFacebookSquare size={30} />
+          <FaInstagram size={30} />
+          <FaGithubSquare size={30} />
+        </div>
+      </div>
 
       <div className="lg:col-span-2 flex justify-between mt-6 w-40 ml-auto ">
-        
         <div>
           <h6 className="font-medium text-gray-800">Support</h6>
           <ul>
@@ -38,11 +41,10 @@ const Footer = () => {
             <Link to="/">
               <li className="py-2 text-gray-800">Guides</li>
             </Link>
-          
+            {firebase.user && <li onClick={firebase.logout}>Log Out</li>}
           </ul>
         </div>
-        
-        </div>
+      </div>
     </div>
   );
 };
